f3a0859433fa46d814f56519823af2bd19f35963
Chris Hatch <hatch@tuta.io>
2017-07-26 15:52:54 +0700

    add contract receipt modal (Receipt) that displays a JSON receipt of contract details, created accounts etc.

---
+++ src/components/Receipt.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Button, Modal} from 'react-bootstrap'
+import CopyToClipboard from 'react-copy-to-clipboard'
+
+const ClipboardCopyButton = ({copied, handleOnClickCopy, receipt}) =>
+  <CopyToClipboard text={receipt} onCopy={handleOnClickCopy}>
+    <span>
+      <Button bsStyle="primary">Copy to Clipboard</Button>
+      {copied && <span style={{marginLeft: 5}}>Copied!</span>}
+    </span>
+  </CopyToClipboard>
+
+const ReceiptModal = ({
+  copied,
+  handleOnClickClose,
+  handleOnClickCopy,
+  receipt,
+  showModal,
+}) =>
+  <Modal show={showModal} onHide={handleOnClickClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Contract Receipt</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <ClipboardCopyButton
+        copied={copied}
+        handleOnClickCopy={handleOnClickCopy}
+        receipt={receipt}
+      />
+      <textarea
+        cols="58"
+        rows="20"
+        defaultValue={receipt}
+        style={{marginTop: '1em'}}
+      />
+    </Modal.Body>
+    <Modal.Footer>
+      <Button onClick={handleOnClickClose}>Close</Button>
+    </Modal.Footer>
+  </Modal>
+
+class Receipt extends React.Component {
+  state = {copied: false, showModal: true}
+
+  handleOnClickClose = () => {
+    this.setState({showModal: false})
+  }
+
+  handleOnClickCopy = () => {
+    this.setState({copied: true})
+  }
+
+  render() {
+    return (
+      <ReceiptModal
+        copied={this.state.copied}
+        handleOnClickClose={this.handleOnClickClose}
+        handleOnClickCopy={this.handleOnClickCopy}
+        receipt={JSON.stringify(this.props.receipt, null, 2)}
+        showModal={this.state.showModal}
+      />
+    )
+  }
+}
+
+Receipt.propTypes = {
+  receipt: PropTypes.object.isRequired,
+}
+
+export default Receipt
